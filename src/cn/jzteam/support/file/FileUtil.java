3b3dd68f9aae47a9c60602e57a53db9d2e4fd41b
jzteam <zhujizhi1012@163.com>
2017-01-20 15:30:48 +0800

    加注释

--- src/cn/jzteam/support/file/FileUtil.java
+++ src/cn/jzteam/support/file/FileUtil.java
@@ -7,7 +7,11 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-
+/**
+ * 文件操作
+ * @author zhujz
+ *
+ */
 public class FileUtil {
     public static void main(String[] args) {
 
